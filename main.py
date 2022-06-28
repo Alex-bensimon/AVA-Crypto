24a1a23dab4e4e174a2cddbc11090eefd88927d9
Alex-bensimon <abensimon@ippon.fr>
2022-06-28 18:27:55 +0200

    🔧 Start to have a look on files

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from src.app.dash.app import launch_app
 
 def main():
     launch_app()
-    return
 
 main()
---
+++ source/src/app/utils/utils.py
@@ -0,0 +1,6 @@
+def get_cqt_api_key():
+    return "ckey_4e20bd1de6b3424c81eefbd7157"
+
+
+def get_base_url_cqt_api():
+    return "https://api.covalenthq.com/v1/"
